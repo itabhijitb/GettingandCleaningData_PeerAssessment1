d9b8ea62a1798e7188e1c046f5098bef817155ca
Abhijit Bhattacharjee <itabhijitb@gmail.com>
2015-11-23 02:26:59 +0530

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # GettingandCleaningData_PeerAssessment1
+
+This is part of the Project work for the Coursera course Getting and Cleaning Data
+
+1. Forkout this project 
+2. Ensure that you have write access to the folder containing the script run_analysis.R
+3. Ensure that you have net connectivity and you are not behind a firewall.
+4. Run the script run_analysis.R
+  4.1 Incase the script fails to download the file
+  
+    4.1.1 Create a directory work under the Script Home directory. (Script Home directory is the path containing the R Script)
+    
+    4.1.2 Download the archive from https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip as getdata-projectfiles-UCI-HAR-Dataset.zip
+    
+    4.1.3 Unzip the content under the work directory
+    
+    4.1.4 Ensure that you have the following folder structure
+    
+```    
+      $ROOT
+        |
+        \-->run_anaysis.R
+        |
+        \-->getdata-projectfiles-UCI-HAR-Dataset
+        |
+        \-->work
+             |
+             \--> UCI HAR Dataset
+                   |
+                   \-->(files in the archive)
+```
+
+    4.1.5 Rerun the SCript.
+5. Ensure that it creates a directory named output under the Script Home folder and it contains a file tidy_data.csv
+6. Open the file tidy_data.csv and verify the content is the same
--- run_analysis.R
+++ run_analysis.R
@@ -1,31 +1,98 @@
-library(data.table)
-download_data <- function()
+
+library(plyr)
+#
+# Constants
+#
+HARUrl = "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+HARZip = "getdata-projectfiles-UCI-HAR-Dataset.zip"
+workdir = "work"
+outputdir = "output"
+TidyCSV = "tidy_data.csv"
+#
+# Cached Data
+#
+columnnames = NULL
+activitynames = NULL
+
+download <- function(overlay = FALSE)
 {
-  download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip ", 
-                "getdata-projectfiles-UCI-HAR-Dataset.zip")
-  unzip("getdata-projectfiles-UCI HAR Dataset.zip")
+  if (!file.exists(workdir))
+  {
+    dir.create(workdir)
+  }
+  if (overlay == TRUE || !file.exists(file.path(workdir, HARZip)))
+  {
+    download.file(HARUrl, destfile = file.path(workdir, HARZip))
+    unzip(file.path(workdir, HARZip), exdir = workdir)
+  }
 }
-#download_data()
-
-DTActivity_names <- read.table('./UCI HAR Dataset/features.txt')
-DTTrain_subjects <- scan('./UCI HAR Dataset/train/subject_train.txt', what = integer(0))
-DTTrain <- read.table('./UCI HAR Dataset/train/X_train.txt')
-setnames(DTTrain, as.character(DTTrain_names$V2))
-DTTrain$subjects <- DTTrain_subjects
-
-DTTest <- read.table('./UCI HAR Dataset/test/X_test.txt')
-setnames(DTTest, as.character(DTTrain_names$V2))
-DTTest_subjects <- scan('./UCI HAR Dataset/test/subject_test.txt', what = integer(0))
-DTTest$subjects <- DTTest_subjects
-
-DTActivity <- rbind(DTTrain, DTTest)
-for(name in DTTrain_names$V2)
+readDS <- function(name)
 {
-    if(!grepl( "mean|std", name))
-    {
-      DTActivity[name] <- NULL
-    }
+  rawDS <- read.table(file.path(
+    workdir,
+    "UCI HAR Dataset",
+    name,
+    paste("X_", name, '.txt', sep = "")
+  ))
+  activities <- read.table(file.path(
+    workdir,
+    "UCI HAR Dataset",
+    name,
+    paste("Y_", name, '.txt', sep = "")
+  ))[[1]]
+  subjects <- read.table(file.path(
+    workdir,
+    "UCI HAR Dataset",
+    name,
+    paste("subject_", name, '.txt', sep = "")
+  ))[[1]]
+  if (is.null(columnnames))
+  {
+    columnnames <- as.character(read.csv(
+      file.path(workdir,
+                "UCI HAR Dataset",
+                "features.txt"),
+      header = FALSE, sep = " "
+    )[[2]])
+  }
+  if (is.null(activitynames))
+  {
+    activitynames <- as.character(read.csv(
+      file.path(workdir,
+                "UCI HAR Dataset",
+                "activity_labels.txt"),
+      header = FALSE, sep = " "
+    )[[2]])
+  }
+  meanStdIndices <- grep("mean\\(\\)|std\\(\\)", columnnames)
+  
+  colnames(rawDS) <- columnnames
+  rawDS <- rawDS[, meanStdIndices]
+  activities <- factor(activities,
+                       sort(unique(activities)),
+                       activitynames)
+  rawDS <- cbind(Subject = subjects, Activity = activities, rawDS)
+  rawDS
 }
-DTActivity_avg <- aggregate(DTActivity, by=list(DTActivity$subjects),FUN=mean)
-DTActivity_avg$Group.1 <- NULL
-write.csv(file = "tidy_data.txt", x = DTActivity_avg)
+
+processDS <- function()
+{
+  trainDS <- readDS('train')
+  testDS <- readDS('test')
+  tidyDS <- rbind(trainDS, testDS)
+  tidyMeans <- ddply(tidyDS, .(Subject, Activity),
+                     function(data) {
+                       colMeans(data[,-c(1,2)])
+                     })
+  names(tidyMeans)[-c(1,2)] <-
+    paste0("Mean-", names(tidyMeans)[-c(1,2)])
+  tidyMeans
+}
+
+download()
+tidyMeans <- processDS()
+if (!file.exists(outputdir))
+{
+  dir.create(outputdir)
+}
+write.table(tidyMeans, file = file.path(outputdir, TidyCSV), row.names = FALSE, sep = ",")
